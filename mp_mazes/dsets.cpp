9ddeb96a141ee542646a5dd258b09ff8a73a11fd
Justin Zhou <jezhou2@illinois.edu>
2020-02-07 18:34:56 -0600

    Update dsets.cpp

---
+++ mp_mazes/dsets.cpp
@@ -0,0 +1,45 @@
+
+/* Your code here! */
+#include <iostream>
+#include <stdio.h>
+#include "dsets.h"
+
+
+void DisjointSets::addelements(int n) {
+    for (int i = 0; i < n; ++i) {
+        contents.push_back(Node(contents.size()));
+    }
+}
+
+int DisjointSets::find(int el) {
+    Node* n = &contents[el];
+    vector<Node*> path;
+    path.push_back(n);
+    while (n->next) {
+        n = n->next;
+        path.push_back(n);
+    }
+    path.pop_back();
+    for (auto it = path.begin(); it != path.end(); it++) {
+        (*it)->next = n;
+    }
+    return n->data;
+}
+
+void DisjointSets::setunion(int a, int b) {
+    int lhN = find(a);
+    int rhN = find(b);
+    Node& A = contents[lhN];
+    Node& B = contents[rhN];
+    if (b > a) {
+        A.next = &B;
+        B.size += A.size;
+        return;
+    }
+    B.next = &A;
+    B.size += A.size;
+}
+
+int DisjointSets::size(int el) {
+    return contents[find(el)].size;
+}
